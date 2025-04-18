0fa52ce97a65aebba7d9753b70ec781ddda0a8fa
Nelson Uhan <nelson@uhan.me>
2025-04-17 21:25:05 -0400

    Implement variants of SimpleModel for error testing

--- test/model.py
+++ test/model.py
@@ -2,7 +2,16 @@ import pyomo.environ as pyo
 
 
 class SimpleModel:
-    def __init__(self):
+    def __init__(
+        self,
+        real_vars=False,
+        nolb_vars=False,
+        noub_vars=False,
+        quad_con=False,
+        nonlinear_con=False,
+        quad_obj=False,
+        nonlinear_obj=False,
+    ):
         cake_types = ['chocolate', 'vanilla', 'matcha']
         ingredients = ['eggs', 'flour']
 
@@ -21,28 +30,50 @@ class SimpleModel:
 
         self.model = pyo.ConcreteModel()
 
+        #
+        # Sets
+        #
         self.model.K = pyo.Set(initialize=cake_types)
         self.model.I = pyo.Set(initialize=ingredients)
 
+        #
+        # Parameters
+        #
         self.model.p = pyo.Param(self.model.K, initialize=prices)
         self.model.b = pyo.Param(self.model.I, initialize=available_ingredients)
         self.model.a = pyo.Param(self.model.I, self.model.K, initialize=recipes)
 
-        self.model.x = pyo.Var(
-            self.model.K, domain=pyo.NonNegativeIntegers, bounds=(0, 100)
-        )
+        #
+        # Variables
+        #
+        if real_vars:
+            x_domain = pyo.Reals
+        else:
+            x_domain = pyo.Integers
 
+        if nolb_vars:
+            x_lb = None
+        else:
+            x_lb = 0
+
+        if noub_vars:
+            x_ub = None
+        else:
+            x_ub = 100
+
+        self.model.x = pyo.Var(self.model.K, domain=x_domain, bounds=(x_lb, x_ub))
+
+        #
+        # Constraints
+        #
         self.model.total_cakes = pyo.Expression(
             expr=pyo.quicksum(self.model.x[k] for k in self.model.K)
         )
 
-        def obj_rule(model):
-            return 150 + sum(model.p[k] * model.x[k] for k in model.K)
-
-        self.model.obj = pyo.Objective(rule=obj_rule, sense=pyo.maximize)
-
         def ingredients_available_rule(model, i):
-            return sum(model.a[i, k] * model.x[k] for k in model.K) <= model.b[i]
+            return (
+                pyo.quicksum(model.a[i, k] * model.x[k] for k in model.K) <= model.b[i]
+            )
 
         self.model.ingredients_available_con = pyo.Constraint(
             self.model.I, rule=ingredients_available_rule
@@ -52,3 +83,57 @@ class SimpleModel:
             return model.total_cakes <= 10
 
         self.model.total_cakes_con = pyo.Constraint(rule=total_cakes_rule)
+
+        def quad_rule(model):
+            return pyo.quicksum(model.x[k] ** 2 for k in model.K) >= 0
+
+        self.model.quad_con = pyo.Constraint(rule=quad_rule)
+
+        def nonlinear_rule(model):
+            return pyo.quicksum(pyo.sin(model.x[k]) for k in model.K) >= 0
+
+        self.model.nonlinear_con = pyo.Constraint(rule=nonlinear_rule)
+
+        if quad_con:
+            self.model.quad_con.activate()
+        else:
+            self.model.quad_con.deactivate()
+
+        if nonlinear_con:
+            self.model.nonlinear_con.activate()
+        else:
+            self.model.nonlinear_con.deactivate()
+
+        #
+        # Objective
+        #
+        def obj_rule(model):
+            return 150 + pyo.quicksum(model.p[k] * model.x[k] for k in model.K)
+
+        self.model.obj = pyo.Objective(rule=obj_rule, sense=pyo.maximize)
+
+        def quad_obj_rule(model):
+            return pyo.quicksum(model.x[k] ** 2 for k in model.K)
+
+        self.model.quad_obj = pyo.Objective(rule=quad_obj_rule, sense=pyo.maximize)
+
+        def nonlinear_obj_rule(model):
+            return pyo.quicksum(pyo.cos(model.x[k]) for k in model.K)
+
+        self.model.nonlinear_obj = pyo.Objective(
+            rule=nonlinear_obj_rule, sense=pyo.maximize
+        )
+
+        self.model.obj.activate()
+        self.model.quad_obj.deactivate()
+        self.model.nonlinear_obj.deactivate()
+
+        if quad_obj:
+            self.model.obj.deactivate()
+            self.model.quad_obj.activate()
+            self.model.nonlinear_obj.deactivate()
+
+        if nonlinear_obj:
+            self.model.obj.deactivate()
+            self.model.quad_obj.deactivate()
+            self.model.nonlinear_obj.activate()
